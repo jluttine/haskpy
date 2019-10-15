8f97ab9dc4eb57ba34b3246e1cefb3a6e41f582b
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-10-15 09:24:22 +0300

    Add default for map in Applicative

--- haskpy/typeclasses/applicative.py
+++ haskpy/typeclasses/applicative.py
@@ -1,10 +1,16 @@
 import attr
 
 from .functor import Functor
+from haskpy.utils import function
 
 
 @attr.s(frozen=True)
 class Applicative(Functor):
+    """Must define at least pure and either apply or apply_to
+
+    Functor methods are given defaults based on Applicative methods.
+
+    """
 
 
     @classmethod
@@ -12,18 +18,27 @@ class Applicative(Functor):
         raise NotImplementedError()
 
 
-
+    @function
     def apply(self, f):
         return f.apply_to(self)
 
 
+    @function
     def apply_to(self, x):
         return x.apply(self)
 
 
+    @function
+    def map(self, f):
+        # Default implementation for Functor based on Applicative
+        return self.pure(f).apply_to(self)
+
+
+@function
 def liftA2(f, x, y):
     return x.map(f).apply(y)
 
 
+@function
 def apply(f, x):
     return x.apply(f)
