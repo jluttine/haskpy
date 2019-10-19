f2231fcf088c720a3b70dcc26ded5522aac13581
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-10-19 15:04:35 +0300

    Move PatternMatchable-related functions

--- haskpy/function.py
+++ haskpy/function.py
@@ -136,3 +136,24 @@ def bind(x, f):
 @function
 def join(x):
     return x.join()
+
+
+#
+# PatternMatchable-related functions
+#
+# NOTE: Currying doesn't work as expected for this function, because this is a
+# generic function and we don't know how many arguments are required. We would
+# first like to get all the required arguments and only after that the actual
+# object on which to pattern match. One solution would be take the patterns as
+# a dictionary. Then this function would always take two arguments and it would
+# be explicit that all the patterns would be given at the same time. Something
+# like:
+#
+# @function
+# def match(patterns, x):
+#     return x.match(**patterns)
+#
+# Is it better than:
+@function
+def match(**kwargs):
+    return lambda x: x.match(**kwargs)
