a53195dd3506d51823f30ad636889939a67dc4ee
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-10-26 09:43:59 +0300

    Add Applicative laws as tests

--- haskpy/typeclasses/applicative.py
+++ haskpy/typeclasses/applicative.py
@@ -1,5 +1,8 @@
 import attr
+from hypothesis import given
+import hypothesis.strategies as st
 
+from haskpy.utils import identity
 from .functor import Functor
 
 
@@ -9,6 +12,94 @@ class _ApplicativeMeta(type(Functor)):
     def pure(cls, x):
         """a -> m a"""
         raise NotImplementedError()
+
+
+    def assert_applicative_identity(cls, v):
+        assert cls.pure(identity).apply_to(v) == v
+        return
+
+
+    @given(st.data())
+    def test_applicative_identity(cls, data):
+        t = cls.assert_applicative_identity
+        t(data.draw(cls.sample(st.integers())))
+        t(data.draw(cls.sample(st.dates())))
+        t(data.draw(cls.sample(cls.sample(st.integers()))))
+        return
+
+
+    def assert_applicative_composition(cls, u, v, w):
+        from haskpy.functions import compose
+        assert (
+            cls.pure(compose).apply_to(u).apply_to(v).apply_to(w) ==
+            u.apply_to(v.apply_to(w))
+        )
+        return
+
+
+    @given(st.data())
+    def test_applicative_composition(cls, data):
+        t = cls.assert_applicative_composition
+        draw = lambda e: data.draw(cls.sample(e))
+        t(
+            # Create some random functions
+            draw(st.integers().map(lambda i: lambda x: x * i)),
+            draw(st.integers().map(lambda i: lambda x: x + i)),
+            draw(st.integers()),
+        )
+        from haskpy.functions import map
+        # Nested structure
+        t(
+            draw(st.integers().map(lambda i: map(lambda x: x * i))),
+            draw(st.integers().map(lambda i: map(lambda x: x + i))),
+            draw(cls.sample(st.integers())),
+        )
+        return
+
+
+    def assert_applicative_homomorphism(cls, f, x):
+        assert cls.pure(f).apply_to(cls.pure(x)) == cls.pure(f(x))
+        return
+
+
+    @given(st.data())
+    def test_applicative_homomorphism(cls, data):
+        cls.assert_applicative_homomorphism(
+            # Create randomish functions
+            data.draw(st.integers().map(lambda i: lambda x: x * i)),
+            data.draw(st.integers())
+        )
+        return
+
+
+    def assert_applicative_interchange(cls, u, y):
+        assert u.apply_to(cls.pure(y)) == cls.pure(lambda f: f(y)).apply_to(u)
+        return
+
+
+    @given(st.data())
+    def test_applicative_interchange(cls, data):
+        cls.assert_applicative_interchange(
+            data.draw(cls.sample(st.integers().map(lambda i: lambda x: x * i))),
+            data.draw(st.integers())
+        )
+        return
+
+
+    def assert_applicative_apply(cls, u, v):
+        from haskpy.functions import apply
+        assert apply(u, v) == v.apply(u)
+        assert v.apply(u) == u.apply_to(v)
+        return
+
+
+    @given(st.data())
+    def test_applicative_apply(cls, data):
+        cls.assert_applicative_apply(
+            data.draw(cls.sample(st.integers().map(lambda i: lambda x: x * i))),
+            data.draw(cls.sample(st.integers()))
+        )
+        return
 
 
 @attr.s(frozen=True)
