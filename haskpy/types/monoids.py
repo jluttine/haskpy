dc939508efcc09e9a3e243ed89546baf5651dd44
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-10-27 13:21:58 +0200

    Move String monoid to monoids module

--- haskpy/types/monoids.py
+++ haskpy/types/monoids.py
@@ -60,3 +60,22 @@ class Or(Monoid, metaclass=_OrMeta):
 
     def append(self, x):
         return Or(self.boolean or x.boolean)
+
+
+class _StringMeta(type(Monoid)):
+
+
+    @property
+    def empty(cls):
+        return cls("")
+
+
+@attr.s(frozen=True, repr=False)
+class String(Monoid, metaclass=_StringMeta):
+
+
+    string = attr.ib()
+
+
+    def append(self, s):
+        return String(self.string + s.string)
