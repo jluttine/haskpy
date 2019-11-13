49b1ece05c97f354eb029fb84d10f386ad4ee138
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-11-13 17:13:37 +0200

    Fix Cartesian implementation and tests

--- haskpy/typeclasses/cartesian.py
+++ haskpy/typeclasses/cartesian.py
@@ -8,130 +8,122 @@ from haskpy import testing
 
 
 class _CartesianMeta(type(Profunctor)):
-    pass
 
 
-    # def sample_type(cls):
-    #     a = testing.sample_type()
-    #     b = testing.sample_type()
-    #     return st.tuples(a, b).map(lambda ab: cls.sample_profunctor_value(a, b))
+    @assert_output
+    def assert_cartesian_identity(cls, h):
+        lunit = lambda a_1: a_1[0]
+        runit = lambda a: (a, ())
+        return (
+            h.dimap(lunit, runit),
+            h.first(),
+        )
 
 
-    # def sample_profunctor_value(cls, a, b):
-    #     return cls.sample_value(a, b)
+    @given(st.data())
+    def test_cartesian_identity(cls, data):
+        # Draw types
+        a = data.draw(testing.sample_hashable_type())
+        b = data.draw(testing.sample_type())
+
+        # Draw values
+        h = data.draw(cls.sample_profunctor_value(a, b))
+
+        cls.assert_cartesian_identity(
+            h,
+            data=data,
+            input_strategy=st.tuples(st.integers(), st.just(())),
+        )
+        return
 
 
-    # @st.composite
-    # def sample_contravariant_value(draw, cls, a):
-    #     b = draw(testing.sample_type())
-    #     return draw(cls.sample_profunctor_value(a, b))
+    @assert_output
+    def assert_cartesian_associativity(cls, h):
+        lassoc = lambda a_bc: ((a_bc[0], a_bc[1][0]), a_bc[1][1])
+        rassoc = lambda ab_c: (ab_c[0][0], (ab_c[0][1], ab_c[1][0]))
+        return (
+            h.first().first().dimap(lassoc, rassoc),
+            h.first(),
+        )
 
 
-    # @st.composite
-    # def sample_functor_value(draw, cls, b):
-    #     a = draw(testing.sample_type())
-    #     return draw(cls.sample_profunctor_value(a, b))
+    @given(st.data())
+    def test_cartesian_associativity(cls, data):
+        # Draw types
+        a = data.draw(
+            st.tuples(
+                testing.sample_hashable_type(),
+                st.tuples(
+                    st.just("foo"),
+                    st.just("bar"),
+                )
+            )
+        )
+        b = data.draw(testing.sample_type())
+
+        # Draw values
+        h = data.draw(cls.sample_profunctor_value(a, b))
+
+        cls.assert_cartesian_associativity(h, data=data, input_strategy=a)
+        return
+
 
     #
-    # Test typeclass laws
+    # Test laws based on default implementations
     #
 
 
-    # @assert_output
-    # def assert_profunctor_identity(cls, x):
-    #     return (
-    #         x,
-    #         x.dimap(identity, identity),
-    #     )
+    @assert_output
+    def assert_cartesian_first(cls, x):
+        return (
+            Cartesian.first(x),
+            x.first(),
+        )
 
 
-    # @given(st.data())
-    # def test_profunctor_identity(cls, data):
-    #     # Draw types
-    #     a = data.draw(testing.sample_hashable_type())
-    #     b = data.draw(testing.sample_type())
+    @given(st.data())
+    def test_cartesian_first(cls, data):
+        # Draw types
+        a1 = data.draw(testing.sample_hashable_type())
+        a2 = data.draw(testing.sample_hashable_type())
+        a = st.tuples(a1, a2)
+        b = data.draw(testing.sample_type())
 
-    #     # Draw values
-    #     x = data.draw(cls.sample_profunctor_value(a, b))
+        # Draw values
+        x = data.draw(cls.sample_profunctor_value(a, b))
 
-    #     cls.assert_profunctor_identity(x, data=data)
-    #     return
+        cls.assert_cartesian_first(
+            x,
+            data=data,
+            input_strategy=a,
+        )
+        return
 
-
-    # #
-    # # Test laws based on default implementations
-    # #
-
-
-    # @assert_output
-    # def assert_profunctor_dimap(cls, x, f, g):
-    #     from haskpy.functions import dimap
-    #     return (
-    #         Profunctor.dimap(x, f, g),
-    #         x.dimap(f, g),
-    #         dimap(f, g, x),
-    #     )
+    @assert_output
+    def assert_cartesian_second(cls, x):
+        return (
+            Cartesian.second(x),
+            x.second(),
+        )
 
 
-    # @given(st.data())
-    # def test_profunctor_dimap(cls, data):
-    #     # Draw types
-    #     b = data.draw(testing.sample_hashable_type())
-    #     c = data.draw(testing.sample_hashable_type())
-    #     d = data.draw(testing.sample_type())
+    @given(st.data())
+    def test_cartesian_second(cls, data):
+        # Draw types
+        a1 = data.draw(testing.sample_hashable_type())
+        a2 = data.draw(testing.sample_hashable_type())
+        a = st.tuples(a1, a2)
+        b = data.draw(testing.sample_type())
 
-    #     # Draw values
-    #     x = data.draw(cls.sample_profunctor_value(b, c))
-    #     f = data.draw(testing.sample_function(b))
-    #     g = data.draw(testing.sample_function(d))
+        # Draw values
+        x = data.draw(cls.sample_profunctor_value(a, b))
 
-    #     cls.assert_profunctor_dimap(x, f, g, data=data)
-    #     return
-
-
-    # @assert_output
-    # def assert_profunctor_map(cls, x, f):
-    #     return (
-    #         Profunctor.map(x, f),
-    #         x.map(f),
-    #     )
-
-
-    # @given(st.data())
-    # def test_profunctor_map(cls, data):
-    #     # Draw types
-    #     b = data.draw(testing.sample_hashable_type())
-    #     c = data.draw(testing.sample_hashable_type())
-    #     d = data.draw(testing.sample_type())
-
-    #     # Draw values
-    #     x = data.draw(cls.sample_profunctor_value(b, c))
-    #     g = data.draw(testing.sample_function(d))
-
-    #     cls.assert_profunctor_map(x, g, data=data)
-    #     return
-
-
-    # @assert_output
-    # def assert_profunctor_contramap(cls, x, f):
-    #     return (
-    #         Profunctor.contramap(x, f),
-    #         x.contramap(f),
-    #     )
-
-
-    # @given(st.data())
-    # def test_profunctor_contramap(cls, data):
-    #     # Draw types
-    #     b = data.draw(testing.sample_hashable_type())
-    #     d = data.draw(testing.sample_type())
-
-    #     # Draw values
-    #     x = data.draw(cls.sample_profunctor_value(b, d))
-    #     f = data.draw(testing.sample_function(b))
-
-    #     cls.assert_profunctor_contramap(x, f, data=data)
-    #     return
+        cls.assert_cartesian_second(
+            x,
+            data=data,
+            input_strategy=a,
+        )
+        return
 
 
 @attr.s(frozen=True)
@@ -151,16 +143,14 @@ class Cartesian(Profunctor, metaclass=_CartesianMeta):
 
 
     def first(self):
-        return self.dimap(_flip_tuple, _flip_tuple).second()
+        """p a b -> p (a, c) (b, c)"""
+        return self.second().dimap(_flip_tuple, _flip_tuple)
 
 
     def second(self):
-        return self.dimap(_flip_tuple, _flip_tuple).first()
+        """p a b -> p (c, a) (c, a)"""
+        return self.first().dimap(_flip_tuple, _flip_tuple)
 
 
 def _flip_tuple(ab):
     return (ab[1], ab[0])
-
-
-# Profunctor-related functions are defined in function module because of
-# circular dependency.
